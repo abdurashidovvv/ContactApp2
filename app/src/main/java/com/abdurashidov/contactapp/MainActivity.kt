52c36d2cca5c4045cc03540ec9d862a75d63c409
Saydullo <abdurashidovsaydullo35@gmail.com>
2022-10-13 14:21:48 +0500

    Send sms

--- app/src/main/java/com/abdurashidov/contactapp/MainActivity.kt
+++ app/src/main/java/com/abdurashidov/contactapp/MainActivity.kt
@@ -3,15 +3,22 @@
 import RvAdapter
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.Intent
 import android.graphics.Color
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.provider.Telephony
+import android.telephony.SmsManager
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.abdurashidov.contactapp.Helper.MyButton
 import com.abdurashidov.contactapp.Helper.MySwipeHelper
@@ -51,11 +58,10 @@ import kotlinx.android.synthetic.main.activity_main.*
                          "Call",
                          30,
                          R.drawable.ic_call,
-                         Color.RED,
+                         Color.WHITE,
                          object : MyButtonClickListener {
                              override fun onClick(position: Int) {
-                                 Toast.makeText(this@MainActivity, "Call", Toast.LENGTH_SHORT)
-                                     .show()
+                                 makeCall(position)
                              }
                          })
                  )
@@ -65,10 +71,10 @@ import kotlinx.android.synthetic.main.activity_main.*
                          "SMS",
                          30,
                          R.drawable.ic_sms,
-                         Color.RED,
+                         Color.WHITE,
                          object : MyButtonClickListener {
                              override fun onClick(position: Int) {
-                                 Toast.makeText(this@MainActivity, "SMS", Toast.LENGTH_SHORT).show()
+                                 sendSms(position)
                              }
                          })
                  )
@@ -118,8 +124,58 @@ import kotlinx.android.synthetic.main.activity_main.*
          }
      }
 
-     private fun makeCall(position: Int) {
+     private fun sendSms(position: Int){
+         askPermission(Manifest.permission.SEND_SMS){
+             val phoneNumber=list[position].number
+             val dialog=AlertDialog.Builder(this).create()
+             val view=layoutInflater.inflate(R.layout.dialog_send_sms, null, false)
+             val name=view.findViewById<TextView>(R.id.name)
+             val number=view.findViewById<TextView>(R.id.number)
+             val text=view.findViewById<TextInputEditText>(R.id.msg)
+             val sendBtn=view.findViewById<CardView>(R.id.send_msg)
+             name.text=list[position].name
+             number.text=list[position].number
+             dialog.setView(view)
+             dialog.show()
+             val sms=SmsManager.getDefault()
+             sendBtn.setOnClickListener {
+                 try {
+                 sms.sendTextMessage(number.text.toString(), null, text.text.toString(), null, null)
+                     dialog.cancel()
+             } catch (ex: Exception) {
+             }
+             }
+         }
+     }
 
+     private fun makeCall(position: Int) {
+        askPermission(Manifest.permission.CALL_PHONE){
+            val phoneNumber=list[position].number
+            val intent=Intent(Intent(Intent.ACTION_CALL))
+            intent.data= Uri.parse("tel:$phoneNumber")
+            startActivity(intent)
+        }.onDeclined {
+                e ->
+            if (e.hasDenied()) {
+
+                AlertDialog.Builder(this)
+                    .setMessage("Ruxsat bermasangiz ilova ishlay olmaydi ruxsat bering...")
+                    .setPositiveButton("yes") { dialog, which ->
+                        e.askAgain();
+                    } //ask again
+                    .setNegativeButton("no") { dialog, which ->
+                        dialog.dismiss();
+                    }
+                    .show();
+            }
+
+            if(e.hasForeverDenied()) {
+                //the list of forever denied permissions, user has check 'never ask again'
+
+                // you need to open setting manually if you really need it
+                e.goToSettings();
+            }
+        }
      }
 
      @SuppressLint("NewApi", "Range")
