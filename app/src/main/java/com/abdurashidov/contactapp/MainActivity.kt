a40a436ed9a10619321d9509ab4d762d5a5d3069
Saydullo <abdurashidovsaydullo35@gmail.com>
2022-10-13 10:10:29 +0500

    Save new contact database and rv adapter

--- app/src/main/java/com/abdurashidov/contactapp/MainActivity.kt
+++ app/src/main/java/com/abdurashidov/contactapp/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.widget.Button
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
@@ -15,19 +16,27 @@ import androidx.recyclerview.widget.RecyclerView
 import com.abdurashidov.contactapp.Helper.MyButton
 import com.abdurashidov.contactapp.Helper.MySwipeHelper
 import com.abdurashidov.contactapp.Listener.MyButtonClickListener
+import com.abdurashidov.contactapp.db.MyDbHelper
 import com.abdurashidov.contactapp.models.Contact
 import com.github.florent37.runtimepermission.kotlin.askPermission
 import com.github.florent37.runtimepermission.kotlin.coroutines.experimental.askPermission
+import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.activity_main.*
 
  class MainActivity : AppCompatActivity() {
 
      private lateinit var adapter:RvAdapter
+     private lateinit var list:ArrayList<Contact>
+     private lateinit var myDbHelper: MyDbHelper
 
      override fun onCreate(savedInstanceState: Bundle?) {
          super.onCreate(savedInstanceState)
          setContentView(R.layout.activity_main)
 
+
+         myDbHelper= MyDbHelper(this)
+         list= ArrayList()
+         list.addAll(myDbHelper.getAllContacts())
 
          rv.setHasFixedSize(true)
 
@@ -41,7 +50,7 @@ import kotlinx.android.synthetic.main.activity_main.*
                      MyButton(this@MainActivity,
                          "Call",
                          30,
-                         R.drawable.ic_launcher_background,
+                         R.drawable.ic_call,
                          Color.RED,
                          object : MyButtonClickListener {
                              override fun onClick(position: Int) {
@@ -55,7 +64,7 @@ import kotlinx.android.synthetic.main.activity_main.*
                      MyButton(this@MainActivity,
                          "SMS",
                          30,
-                         R.drawable.ic_launcher_background,
+                         R.drawable.ic_sms,
                          Color.RED,
                          object : MyButtonClickListener {
                              override fun onClick(position: Int) {
@@ -69,6 +78,44 @@ import kotlinx.android.synthetic.main.activity_main.*
 
          readContact()
 
+     }
+
+     override fun onResume() {
+         super.onResume()
+
+         add.setOnClickListener {
+             val dialog=AlertDialog.Builder(this).create()
+             val view=layoutInflater.inflate(R.layout.dialog_add_contact, null, false)
+             val editTextName=view.findViewById<TextInputEditText>(R.id.name)
+             val editTextNumber=view.findViewById<TextInputEditText>(R.id.number)
+             val btnSave=view.findViewById<Button>(R.id.btn_save)
+             dialog.setView(view)
+             dialog.show()
+
+             btnSave.setOnClickListener {
+                 if (editTextName.text.toString().isNotEmpty() && editTextNumber.text.toString().isNotEmpty()){
+                     val contact=Contact(
+                         name = editTextName.text.toString(),
+                         number = editTextNumber.text.toString()
+                     )
+                     list.add(contact)
+                     adapter.notifyDataSetChanged()
+                     dialog.cancel()
+                 }else{
+                     Toast.makeText(this, "Iltimos hamma maydonlarni toldiring.", Toast.LENGTH_SHORT).show()
+                 }
+             }
+         }
+     }
+
+     override fun onStop() {
+         super.onStop()
+         list.forEach {
+             if (myDbHelper.getAllContacts().contains(it)){
+             }else{
+                 myDbHelper.addContact(it)
+             }
+         }
      }
 
      private fun makeCall(position: Int) {
@@ -88,7 +135,14 @@ import kotlinx.android.synthetic.main.activity_main.*
                 contactList.add(contact)
             }
              contacts.close()
-             adapter=RvAdapter(contactList)
+
+             contactList.forEach {
+                 if (!myDbHelper.getAllContacts().contains(it)){
+                     list.add(it)
+                 }
+             }
+
+             adapter=RvAdapter(list)
              rv.adapter=adapter
          }.onDeclined { e->
              if(e.hasDenied()){
